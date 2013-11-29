b9cc1a90d49b0f17f976d5a41e1338c56785f7ce
Oshane Bailey <b4.oshany@gmail.com>
2013-11-28 19:49:49 -0500

    'change the file structure'

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <h1>Info2180 folder</h1>
 
-<a href="info2180/lab7/index.php">Lab 7</a>
-<a href="info2180/lab8/index.php">Lab 8</a>
-<a href="info2180/proj1/index.php">Project 1</a>
+<a href="info2180/lab7/">Lab 7</a>
+<a href="info2180/lab8/">Lab 8</a>
+<a href="info2180/proj1/">Project 1</a>
 <h1>MDIA3005</h1>
-<a href="mdia3005/blog/index.php">Blog</a>
-<a href="mdia3005/wordpress/index.php">Wordpress</a>
+<a href="mdia3005/blog/">Blog</a>
+<a href="mdia3005/wordpress/">Wordpress</a>
